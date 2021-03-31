077d2a25b29e4be2be55116383ee9eaf3c743da3
Lee_Muxian <lee0906926618@gmail.com>
2021-03-31 10:04:48 -0700

    add .

--- hw2_code.cpp
+++ hw2_code.cpp
@@ -8,30 +8,25 @@ uLCD_4DGL uLCD(D1, D0, D2);
 AnalogOut wave_out(D7);
 AnalogIn sample_wave(A5);
 Thread thread;
-//Timer t;
 float ADCdata[500];
 
 void sample(void)
 {
-    //t.start();
     for(int i = 0; i < 500; i++) {
         ADCdata[i] = sample_wave;
         ThisThread::sleep_for(2ms);
     }
-    //t.stop();
-    //auto s= chrono::duration_cast<chrono::seconds>(t.elapsed_time()).count();
     for (int i = 0; i < 500; i++) {
         printf("%f\r\n", ADCdata[i]);
-        //printf("Timer time: %llus\n", s);
     }
 }
 
 int main()
 {
-    float freq = 82.6, amp;
+    int freq = 826;
     int i, j;
-    double T, up_time, down_time;   // T means the period of triangular wave.
-                                    // up_time means the time of the upward region of one period. 
+    double T, up_time, down_time, amp;      // T means the period of triangular wave.
+                                            // up_time means the time of the upward region of one period. 
 
     wave_out = 0;
 
@@ -44,27 +39,27 @@ int main()
     uLCD.color(BLACK);
     uLCD.set_font(FONT_5X7);
     uLCD.locate(i, j);
-    uLCD.printf("%.1f Hz", freq);
+    uLCD.printf("%d Hz", freq);
 
     while(1) {
         if (Up == 1) {
             if (freq > 900)
-                freq = 82.6;
+                freq = 126;
             else
                 freq += 100;
             uLCD.cls();
             uLCD.locate(i, j);
-            uLCD.printf("%.1f Hz", freq);
+            uLCD.printf("%d Hz", freq);
             ThisThread::sleep_for(500ms);
         }
         else if (Down == 1) {
-            if (freq < 100)
-                freq = 982.6;
+            if (freq < 200)
+                freq = 826;
             else
                 freq -= 100;
             uLCD.cls();
             uLCD.locate(i, j);
-            uLCD.printf("%.1f Hz", freq);
+            uLCD.printf("%d Hz", freq);
             ThisThread::sleep_for(500ms);
         }
         if (Select == 1)
@@ -79,10 +74,10 @@ int main()
     thread.start(sample);
 
     while(1) {
-        for(float a = 0.0; a < amp; a += amp / (up_time / 0.0000237)) {     // we try and error and find out that 0.0000237 is the best value.
+        for(float a = 0.0; a < amp; a += amp / (up_time / 0.0000241)) {     // we try and error and find out that 0.0000237 is the best value.
             wave_out = a;
         }
-        for(float b = amp; b > 0; b -= amp / (down_time / 0.0000237)) {
+        for(float b = amp; b > 0; b -= amp / (down_time / 0.0000241)) {
             wave_out = b;
         }
     }
